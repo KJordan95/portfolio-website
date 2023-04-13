513c6a6e75aeeb072c1b954d32e9d940e7d7305b
KJordan95 <kameronjordan95@gmail.com>
2023-04-13 15:11:00 -0500

    created resume page with download link

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,8 @@
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import { NavBar } from "./components/navbar";
-import { About } from "./pages/home/home";
+import { Home } from "./pages/home/home";
 import "./App.css";
+import { Resume } from "./pages/resume/resume";
 
 function App() {
   return (
@@ -9,7 +10,8 @@ function App() {
       <Router>
         <NavBar />
         <Routes>
-          <Route path="/" element={<About />} />
+          <Route path="/" element={<Home />} />
+          <Route path="/resume" element={<Resume />} />
         </Routes>
       </Router>
     </div>
--- src/pages/home/home.tsx
+++ src/pages/home/home.tsx
@@ -24,7 +24,7 @@ export interface ImageSectionInterface {
   order: number;
 }
 
-export const About = () => {
+export const Home = () => {
   let cardCounter = 0;
   let frameworkCounter = 0;
 
