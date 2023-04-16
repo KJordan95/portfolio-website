5d8f7783876d9252c18ea94c4f7ef7d66bd8357c
KJordan95 <kameronjordan95@gmail.com>
2023-04-16 14:42:00 -0500

    passing work experience info as props

--- src/components/sections/experience-details.tsx
+++ src/components/sections/experience-details.tsx
@@ -1,41 +1,21 @@
-export const ExperienceDeails = () => {
+import React from "react";
+
+interface ExperienceDeailsProps {
+  title: string;
+  company: string;
+  dateRange: string;
+  children: React.ReactNode;
+}
+
+export const ExperienceDeails = (props: ExperienceDeailsProps) => {
   return (
     <div className="work-details">
       <h3 className="work-details-title">
-        <span className="work-details-role">Role - </span>
-        <span className="work-details-compamy">Company</span>
+        <span className="work-details-role">{props.title} - </span>
+        <span className="work-details-compamy">{props.company}</span>
       </h3>
-      <p className="work-details-date-range">StartDate to Current</p>
-      <div>
-        <ul>
-          <li>Bullet 1</li>
-          <li>Bullet 2</li>
-          <li>
-            {" "}
-            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-            eiusmod tempor incididunt ut labore et dolore magna aliqua. Vitae
-            congue mauris rhoncus aenean vel elit scelerisque. Faucibus purus in
-            massa tempor. Nascetur ridiculus mus mauris vitae ultricies. Augue
-            lacus viverra vitae congue eu. Consectetur adipiscing elit ut
-            aliquam purus. Tincidunt arcu non sodales neque. Risus quis varius
-            quam quisque id diam vel quam elementum. Quis eleifend quam
-            adipiscing vitae proin sagittis nisl. Metus aliquam eleifend mi in
-            nulla posuere. Ipsum faucibus vitae aliquet nec ullamcorper sit.
-            Pharetra massa massa ultricies mi quis hendrerit. Lorem ipsum dolor
-            sit amet, consectetur adipiscing elit, sed do eiusmod tempor
-            incididunt ut labore et dolore magna aliqua. Vitae congue mauris
-            rhoncus aenean vel elit scelerisque. Faucibus purus in massa tempor.
-            Nascetur ridiculus mus mauris vitae ultricies. Augue lacus viverra
-            vitae congue eu. Consectetur adipiscing elit ut aliquam purus.
-            Tincidunt arcu non sodales neque. Risus quis varius quam quisque id
-            diam vel quam elementum. Quis eleifend quam adipiscing vitae proin
-            sagittis nisl. Metus aliquam eleifend mi in nulla posuere. Ipsum
-            faucibus vitae aliquet nec ullamcorper sit. Pharetra massa massa
-            ultricies mi quis hendrerit.";
-          </li>
-          <li>Bullet 4</li>
-        </ul>
-      </div>
+      <p className="work-details-date-range">{props.dateRange}</p>
+      <div>{props.children}</div>
     </div>
   );
 };
--- src/components/sections/experience-section.tsx
+++ src/components/sections/experience-section.tsx
@@ -29,10 +29,60 @@ export const ExperienceSection = () => {
         </div>
         <div className="experience-container">
           <TabContent id={"company1"} activeTab={activeTab}>
-            <ExperienceDeails />
+            <ExperienceDeails
+              title={"Role"}
+              company={"Company"}
+              dateRange={"StartDate -EndDate"}
+            >
+              <ul>
+                <li>Bullet 1</li>
+                <li>Bullet 2</li>
+                <li>
+                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed
+                  do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+                  Vitae congue mauris rhoncus aenean vel elit scelerisque.
+                  Faucibus purus in massa tempor. Nascetur ridiculus mus mauris
+                  vitae ultricies. Augue lacus viverra vitae congue eu.
+                  Consectetur adipiscing elit ut aliquam purus. Tincidunt arcu
+                  non sodales neque. Risus quis varius quam quisque id diam vel
+                  quam elementum. Quis eleifend quam adipiscing vitae proin
+                  sagittis nisl. Metus aliquam eleifend mi in nulla posuere.
+                  Ipsum faucibus vitae aliquet nec ullamcorper sit. Pharetra
+                  massa massa ultricies mi quis hendrerit. Lorem ipsum dolor sit
+                  amet, consectetur adipiscing elit, sed do eiusmod tempor
+                  incididunt ut labore et dolore magna aliqua. Vitae congue
+                  mauris rhoncus aenean vel elit scelerisque. Faucibus purus in
+                  massa tempor. Nascetur ridiculus mus mauris vitae ultricies.
+                  Augue lacus viverra vitae congue eu. Consectetur adipiscing
+                  elit ut aliquam purus. Tincidunt arcu non sodales neque. Risus
+                  quis varius quam quisque id diam vel quam elementum. Quis
+                  eleifend quam adipiscing vitae proin sagittis nisl. Metus
+                  aliquam eleifend mi in nulla posuere. Ipsum faucibus vitae
+                  aliquet nec ullamcorper sit. Pharetra massa massa ultricies mi
+                  quis hendrerit.";
+                </li>
+                <li>Bullet 4</li>
+              </ul>
+            </ExperienceDeails>
           </TabContent>
           <TabContent id={"company2"} activeTab={activeTab}>
-            <p>Tab2 Works</p>
+            <ExperienceDeails
+              title={"Inventory Associate"}
+              company={"WIS International"}
+              dateRange={"MAY 2018 - JUNE 2021"}
+            >
+              <ul>
+                <li>
+                  Monitored inventory levels and identified discrepancies,
+                  ensuring accuracy of stock data
+                </li>
+                <li>
+                  Inspected supplies and product inventories to assess quality
+                  and quantity, returning unsuitable materials to vendors
+                </li>
+                <li>Trained 30+ employees on equipment used</li>
+              </ul>
+            </ExperienceDeails>
           </TabContent>
         </div>
       </div>
